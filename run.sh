268ec7e933f0242cdde8b8f0766cb1c1f467f593
jonycodes <mr.joannier@yahoo.com>
2018-01-23 14:44:09 -0500

    fixed bash script

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 docker run --name cryptdb_v1 -p 3306:3306 -p 3399:3399 -e MYSQL_ROOT_PASSWORD='letmein'  cryptbdb:1.0 &
 
+sleep 5
+
 docker exec -it cryptdb_v1 bash
 
-docker stop $(docker ps -a -q)
+docker stop cryptdb_v1
 
-docker rm $(docker ps -a -q -f STATUS=exited)
+docker rm cryptdb_v1
