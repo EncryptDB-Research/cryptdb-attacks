0acbbcb7882463d19142f0aec257c1aff1e0c33a
jonycodes <mr.joannier@yahoo.com>
2018-01-23 14:30:46 -0500

    fixing bash error

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ docker run --name cryptdb_v1 -p 3306:3306 -p 3399:3399 -e MYSQL_ROOT_PASSWORD='l
 
 docker exec -it cryptdb_v1 bash
 
-docker stop $(docker ps -a)
+docker stop $(docker ps -a -q)
 
 docker rm $(docker ps -a -q -f STATUS=exited)
